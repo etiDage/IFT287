5180aa3633dacaf3afe05f78fb19f342423075ac
Charles <charlybelan@gmail.com>
2020-11-02 14:39:01 -0500

    ajout de fonctions de GestionLot.java pour objectBD

--- TP3/JardinCollectif/src/JardinCollectif/GestionLot.java
+++ TP3/JardinCollectif/src/JardinCollectif/GestionLot.java
@@ -1,5 +1,7 @@
 package JardinCollectif;
 
+import java.sql.SQLException;
+
 public class GestionLot {
 	private Connexion cx;
 	private TableLots tableLots;
@@ -64,4 +66,80 @@ public class GestionLot {
 		}
 
 	}
+	
+	public void rejoindreLot(int noMembre, String nomLot) throws Exception
+	{
+		try
+		{
+			cx.demarreTransaction();
+			if(!tableMembres.exist(noMembre))
+			{
+				throw new IFT287Exception("Le noMembre " + noMembre + " n'existe pas dans la liste de membre");
+			}
+			if(!tableLots.exist(nomLot))
+			{
+				throw new IFT287Exception("Le lot " + nomLot + " n'existe pas dans la liste de lot");
+			}
+			if(tableLots.exist(noMembre, nomLot))
+			{
+				throw new IFT287Exception("Une demande existe deja pour le membre " + noMembre + " au lot " + nomLot);
+			}
+			tableLots.ajouterDemandes(noMembre, nomLot);
+			
+			cx.commit();
+		}
+		catch(Exception e)
+		{
+			cx.rollback();
+			throw e;
+		}
+	}
+	
+	public void accepterDemande(int noMembre, String nomLot) throws Exception
+	{
+		try
+		{
+			cx.demarreTransaction();
+			if(!tableLots.existDemande(noMembre, nomLot))
+			{
+				throw new IFT287Exception("Aucune demande appartient au noMembre " + noMembre + " et au lot " + nomLot);
+			}
+			if(tableLots.getNbMembreLot(nomLot) >= tableLots.getNbMaxMembre(nomLot))
+			{
+				throw new IFT287Exception("Le lot " + nomLot + " est a son nombre maximal de membre");
+			}
+			if(tableLots.existAssignation(noMembre, nomLot))
+			{
+				throw new IFT287Exception("Le membre " + noMembre + " est deja assigner au lot " + nomLot);
+			}
+			tableLots.ajouterAssignations(noMembre, nomLot);
+			tableLots.supprimerDemande(noMembre, nomLot);
+			
+			cx.commit();
+		}
+		catch(Exception e)
+		{
+			cx.rollback();
+			throw e;
+		}
+
+	}
+	
+	public void refuserDemande(int noMembre, String nomLot) throws SQLException
+	{
+		try
+		{
+			cx.demarreTransaction();
+			tableLots.supprimerDemande(noMembre, nomLot);
+			
+			cx.commit();
+		}
+		catch(Exception e)
+		{
+			cx.rollback();
+			throw e;
+		}
+
+	}
+	
 }
