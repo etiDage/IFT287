dc0f682fddc35e1f2b52508cbd6e235773ba0b50
dage1001 <etienne.dagenais@usherbrooke.ca>
2020-12-03 10:25:40 -0500

    modifications

--- WEB-INF/src/jardinServlet/AjoutPlante.java
+++ WEB-INF/src/jardinServlet/AjoutPlante.java
@@ -10,12 +10,13 @@ import javax.servlet.http.*;
 import JardinCollectif.IFT287Exception;
 import JardinCollectif.GestionJardin;
 
-@Deprecated
-public class AjoutPlante extends HttpsServlet {
+
+public class AjoutPlante extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 		
 		@Override
-		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+		{
 			
 			Integer etat =(Integer) request.getSession().getAttribute("etat");
 			if(etat ==null) {
@@ -23,7 +24,7 @@ public class AjoutPlante extends HttpsServlet {
 				dispatcher.forward(request, response);
 			}
 			else
-				try
+			try
 			{
 					String nomPlante = request.getParameter("nomPlante");
 					request.setAttribute("nomPlante",nomPlante);
@@ -47,14 +48,21 @@ public class AjoutPlante extends HttpsServlet {
 	                dispatcher.forward(request, response);
 					
 					
-			}catch (IFT287Exception e)
+			}
+			catch (IFT287Exception e)
             {
                 List<String> listeMessageErreur = new LinkedList<String>();
                 listeMessageErreur.add(e.toString());
                 request.setAttribute("listeMessageErreur", listeMessageErreur);
-                RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/emprunt.jsp");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/ajouterPlante.jsp");
                 dispatcher.forward(request, response);
             }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
+            }
+
 		}
 	    @Override
 	    public void doPost(HttpServletRequest request, HttpServletResponse response)
